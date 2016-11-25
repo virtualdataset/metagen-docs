96828531942893481d900de362025945869bcec8
Jonathan Shook <jshook@gmail.com>
2016-11-25 17:05:37 -0600

    addign publish script

---
+++ publish.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if (( $(git status --porcelain 2>/dev/null| wc -l) > 0 ))
+then
+ printf "Unchanged files are present. Please clean up your working tree first.\n" ;
+ git status
+ exit 2
+fi
+mdbook build docs
+git add -u docs
+git commit -m'publishing docs'
+git push
